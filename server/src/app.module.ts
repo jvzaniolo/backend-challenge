9b8764ab8e7f46c9feec65ab25bec224aab987fd
João Vitor <jv.zaniolo@gmail.com>
2025-05-05 11:01:51 -0300

    feat: environment variables

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,5 +1,6 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CustomUuidScalar } from './@core/scalar/uuid.scalar';
@@ -8,6 +9,9 @@ import { SubmissionsModule } from './submissions/submissions.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'schema.gql',
@@ -18,12 +22,8 @@ import { SubmissionsModule } from './submissions/submissions.module';
       },
     }),
     TypeOrmModule.forRoot({
+      url: process.env.DATABASE_URL,
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'docker',
-      password: 'docker',
-      database: 'rocketseat_challenge',
       autoLoadEntities: true,
       synchronize: process.env.NODE_ENV !== 'production',
     }),
