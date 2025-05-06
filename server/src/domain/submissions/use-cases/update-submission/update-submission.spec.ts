44e4f0f8d7b6be8b93aae31b37c4b08bd4b2e606
João Vitor <jv.zaniolo@gmail.com>
2025-05-06 12:13:26 -0300

    test: update submission use case

---
+++ server/src/domain/submissions/use-cases/update-submission/update-submission.spec.ts
@@ -0,0 +1,69 @@
+import { Test } from '@nestjs/testing';
+import { randomUUID } from 'node:crypto';
+import { SubmissionStatus } from '../../entities/submission.entity';
+import { FakeSubmissionsRepository } from '../../repositories/fake/fake-submissions.repository';
+import { SubmissionsRepository } from '../../repositories/typeorm/submissions.repository';
+import { UpdateSubmissionUseCase } from './update-submission';
+
+describe('Update submission use case', () => {
+  let sut: UpdateSubmissionUseCase;
+  let submissionsRepository: SubmissionsRepository;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        UpdateSubmissionUseCase,
+        {
+          provide: SubmissionsRepository,
+          useClass: FakeSubmissionsRepository,
+        },
+      ],
+    }).compile();
+
+    sut = module.get(UpdateSubmissionUseCase);
+    submissionsRepository = module.get(SubmissionsRepository);
+  });
+
+  it('should be defined', () => {
+    expect(sut).toBeDefined();
+  });
+
+  it('should update a submission', async () => {
+    const challengeId = randomUUID();
+    const newSubmission = await submissionsRepository.create({
+      challengeId,
+      repositoryUrl: 'https://github.com/user/repo',
+    });
+
+    await expect(submissionsRepository.findMany({ challengeId, perPage: 10 })).resolves.toEqual(
+      expect.objectContaining({
+        items: [
+          expect.objectContaining({
+            repositoryUrl: 'https://github.com/user/repo',
+            status: SubmissionStatus.Pending,
+            grade: null,
+          }),
+        ],
+      }),
+    );
+
+    const { submission } = await sut.execute({
+      submissionId: newSubmission.id,
+      status: SubmissionStatus.Done,
+      grade: 8,
+    });
+
+    expect(submission).toBeDefined();
+    await expect(submissionsRepository.findMany({ challengeId, perPage: 10 })).resolves.toEqual(
+      expect.objectContaining({
+        items: [
+          expect.objectContaining({
+            repositoryUrl: 'https://github.com/user/repo',
+            grade: 8,
+            status: SubmissionStatus.Done,
+          }),
+        ],
+      }),
+    );
+  });
+});
