f6a6317e8fd34ed4e5109b36b907a0f0fb2ede34
João Vitor <jv.zaniolo@gmail.com>
2025-05-07 09:35:14 -0300

    feat(submit-challenge): resolve challenge field on query

--- server/src/infra/http/graphql/resolvers/submit-challenge.resolver.ts
+++ server/src/infra/http/graphql/resolvers/submit-challenge.resolver.ts
@@ -1,10 +1,15 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Parent, ResolveField, Resolver } from '@nestjs/graphql';
+import { ChallengesRepository } from '~/domain/challenges/repositories/challenges.repository';
 import { SubmitChallengeUseCase } from '~/domain/submissions/use-cases/submit-challenge/submit-challenge';
+import { ChallengeType } from '../object-types/challenge.type';
 import { SubmissionType } from '../object-types/submission.type';
 
 @Resolver(() => SubmissionType)
 export class SubmitChallengeResolver {
-  constructor(private readonly submitChallengeUseCase: SubmitChallengeUseCase) {}
+  constructor(
+    private readonly submitChallengeUseCase: SubmitChallengeUseCase,
+    private readonly challengesRepository: ChallengesRepository,
+  ) {}
 
   @Mutation(() => SubmissionType)
   async submitChallenge(
@@ -16,5 +21,10 @@ export class SubmitChallengeResolver {
       repositoryUrl,
     });
     return submission;
+  }
+
+  @ResolveField(() => ChallengeType)
+  async challenge(@Parent() submission: SubmissionType) {
+    return this.challengesRepository.findById(submission.challengeId);
   }
 }
